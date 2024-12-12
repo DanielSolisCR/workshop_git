1e93cffdf69a04c299017c8e17ae2e9ce7ee3341
DanielSolisCR <d.solislara777@gmail.com>
2024-12-11 23:12:31 -0600

    Imprimiendo valores

--- Python/ejercicio1.py
+++ Python/ejercicio1.py
@@ -3,3 +3,10 @@ libros = {
     "To Kill a Mockingbird": {"autor": "Harper Lee", "año": 1960, "género": "Ficción", "calificación": 4.9},
     "El Gran Gatsby": {"autor": "F. Scott Fitzgerald", "año": 1925, "género": "Ficción", "calificación": 4.3}
 }
+
+libro = "1984"
+print(f"Detalles del libro '{libro}':")
+print(f"Autor: {libros[libro]['autor']}")
+print(f"Año de publicación: {libros[libro]['año']}")
+print(f"Género: {libros[libro]['género']}")
+print(f"Calificación: {libros[libro]['calificación']}")
